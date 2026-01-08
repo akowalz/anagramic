61e072aba08d417c8cca733abbe6c526f8c2ad5c
Alex Kowalczuk <askowalczuk93@gmail.com>
2026-01-08 10:41:40 -0600

    refactor app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,44 @@
 import "./App.css";
 
-import { useState } from "react";
+import { useState, type JSX } from "react";
 import TileCanvas from "./TileCanvas/TileCanvas";
 
 import Form from "./Form/Form";
 
+type Tool = "Tiles" | "Line";
+
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [letters, setLetters] = useState<string[]>([]);
+  const [tool, setTool] = useState<Tool>("Tiles");
 
   const resetLetters = () => {
     setInputValue("");
     setLetters([]);
   };
+
+  function getContainerView(): JSX.Element {
+    if (letters.length === 0) {
+      return (
+        <Form
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          setLetters={setLetters}
+        />
+      );
+    }
+
+    switch (tool) {
+      case "Tiles":
+        return <TileCanvas letters={letters} resetLetters={resetLetters} />;
+
+      case "Line":
+        return <div>Line tool</div>;
+
+      default:
+        throw `unknown tool ${tool}`;
+    }
+  }
 
   return (
     <>
@@ -20,19 +46,7 @@ function App() {
         <h1>annagram thing</h1>
       </div>
 
-      <div id="main-container">
-        {letters.length === 0 && (
-          <Form
-            inputValue={inputValue}
-            setInputValue={setInputValue}
-            setLetters={setLetters}
-          />
-        )}
-
-        {letters.length > 0 && (
-          <TileCanvas letters={letters} resetLetters={resetLetters} />
-        )}
-      </div>
+      <div id="main-container">{getContainerView()}</div>
     </>
   );
 }
